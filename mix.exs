6d21bc37dad8d92c26aa391f06e850c60d993bb0
Tyler Cain <tyler.cain@revzilla.com>
2019-03-05 10:01:55 -0500

    adjust mix for release

--- mix.exs
+++ mix.exs
@@ -1,31 +1,39 @@
 defmodule FTPoison.Mixfile do
   use Mix.Project
 
+  @version 0.1.0
+
   def project do
     [app: :ftpoison,
-     version: "0.0.1",
-     elixir: "~> 1.2",
+     version: @version,
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     name: "FTPoison",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp description do
+    """
+    Simple Elixir wrapper for the erlang :ftp library.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["RevZilla", "Tyler Cain", "Steve DeGele"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/revzilla/ftpoison"},
+      files: ~w(lib mix.exs README.md CHANGELOG.md)
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end

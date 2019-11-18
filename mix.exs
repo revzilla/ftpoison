8518d9f8469bdce25edcde0ead6afec09a7221c2
Steve <steve.degele@revzilla.com>
2019-11-18 13:00:07 -0500

    bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FTPoison.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [app: :ftpoison,
@@ -37,6 +37,8 @@ defmodule FTPoison.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
+    ]
   end
 end
